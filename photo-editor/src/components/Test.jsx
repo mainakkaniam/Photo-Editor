9d95fc84690a1219a06642cea3b45219872e613b
mainak <heellomeelo@gmail.com>
2023-09-16 10:16:15 +0530

    upload image feature added

--- photo-editor/src/components/Test.jsx
+++ photo-editor/src/components/Test.jsx
@@ -1,3 +1,4 @@
+import { Button } from '@mui/material';
 import React from 'react';
 import ImageUploading from 'react-images-uploading';
 
@@ -13,45 +14,7 @@ export default function Test() {
 
   return (
     <div className="App">
-      <ImageUploading
-        multiple
-        value={images}
-        onChange={onChange}
-        maxNumber={maxNumber}
-        dataURLKey="data_url"
-      >
-        {({
-          imageList,
-          onImageUpload,
-          onImageRemoveAll,
-          onImageUpdate,
-          onImageRemove,
-          isDragging,
-          dragProps,
-        }) => (
-          // write your building UI
-          <div className="upload__image-wrapper">
-            <button
-              style={isDragging ? { color: 'red' } : undefined}
-              onClick={onImageUpload}
-              {...dragProps}
-            >
-              Click or Drop here
-            </button>
-            &nbsp;
-            <button onClick={onImageRemoveAll}>Remove all images</button>
-            {imageList.map((image, index) => (
-              <div key={index} className="image-item">
-                <img src={image['data_url']} alt="" width="100" />
-                <div className="image-item__btn-wrapper">
-                  <button onClick={() => onImageUpdate(index)}>Update</button>
-                  <button onClick={() => onImageRemove(index)}>Remove</button>
-                </div>
-              </div>
-            ))}
-          </div>
-        )}
-      </ImageUploading>
+<Button variant="contained" component="label">Upload image<input type="file" accept="image/*" hidden/></Button>
     </div>
   );
 }
